7a63b582e5bfb74d5eb436765aa366ab9b130a85
JamieW87 <jamie.w101@gmail.com>
2019-02-20 17:15:53 +0000

    added signup form

--- main.go
+++ main.go
@@ -7,29 +7,55 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
+//Global variables
+var (
+	homeView    *views.View
+	contactView *views.View
+	faqView     *views.View
+	signupView  *views.View
+)
 
 func main() {
+	//Builds our views when the program first starts running.
+	//Calls the NewView function, passes it the bootstrap template and the page it wants it to open.
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
+	//Handlefuncs tell which route starts which func.
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
 }
 
+/*Below are the functions that deal with the requests from the browser.*/
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
-	if err != nil {
-		panic(err)
-	}
+	//So now the Render function deals with executing the template to keep the functions clean
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(faqView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
+// A helper function that panics on any error
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
--- views/view.go
+++ views/view.go
@@ -1,25 +1,54 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+//Declaring global variables
+//LayoutDir specifies the layout directory and TemplateExt states the extension we want our files to match
+var (
+	LayoutDir   = "views/layouts/"
+	TemplateExt = ".gohtml"
+)
+
+//View declares the View type
+type View struct {
+	Template *template.Template
+	Layout   string
+}
 
 //NewView runs logic for all views
 /*This function handles appending common template files to the list of files provided*/
+//Func main calls this function, passes to it the layout (a string) and the files it must load.
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml")
+	//parses the files in the layout folder as set out in layoutFiles function
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
+	//This function then returns the parsed templates, and the layout that was passed to it.
 	return &View{
 		Template: t,
 		Layout:   layout,
 	}
 }
 
-//View declares the View type
-type View struct {
-	Template *template.Template
-	Layout   string
+//Uses the global variables above and returns a slice of a string.
+func layoutFiles() []string {
+	//States the templates we are going to include in our view, Anything in the layoutDir, with the filename all (*) with
+	//the extension that matches TemplateExt = Basically creates the variable files which equals "views/layouts/*.gohtml"
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	//Returns the files variable set above, as a slice of string.
+	return files
+}
+
+//Render method added to the View type (Why the view part comes before the function name)
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
